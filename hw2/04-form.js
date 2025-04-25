534de12be4e8d5147ea220e95f2fb7e982bf11d7
Delanie <degraham@pdx.edu>
2025-04-24 19:29:21 -0700

    Updated form to match directions.

--- hw2/04-form.js
+++ hw2/04-form.js
@@ -23,10 +23,18 @@ app.get("/form", (req, res) => {
   res.status(200);
   res.set({ "Content-Type": "text/html" });
   res.write('<form action="/submit" method="post">');
-  res.write('<label for="username">Username: </label>');
-  res.write('<input type="text" name="username" id="username"><br>');
+  res.write('<label for="name">Name: </label>');
+  res.write('<input type="text" name="name" id="name"><br>');
   res.write('<label for="email">Email: </label>');
   res.write('<input type="email" name="email" id="email"><br>');
+  res.write('<label for="comments">Comments: </label>');
+  res.write('<input type="text" name="comments" id="comments"><br>');
+  
+  res.write('<label for="newsletter">Newsletter: </label><br>');
+  res.write('<input type="radio" name="newsletter" id="yes" value="yes">');
+  res.write('<label for="yes">Yes, sign me up for the newsletter."</label><br />');
+  res.write('<input type="radio" name="newsletter" id="no" value="no">');
+  res.write('<label for="no">No, thank you.</label><br />');
   res.write('<input type="submit">');
   res.write("</form>");
   res.end();
@@ -35,8 +43,14 @@ app.get("/form", (req, res) => {
 app.post("/submit", (req, res) => {
   res.status(200);
   res.set({ "Content-Type": "text/html" });
-  res.write(`${req.body.username} - ${req.body.email}`);
-  console.log(`${req.body.username} - ${req.body.email}`);
+  res.write(`Name: ${req.body.name} <br />`);
+  res.write(`Email: ${req.body.email} <br />`);
+  res.write(`Comments: ${req.body.comments ? req.body.comments : "n/a" }<br />`);
+  if (req.body.newsletter === 'yes') {
+    res.write("Newsletter: Yes, sign me up for the newsletter.")
+  } else {
+    res.write(`Newsletter: No, thank you.`); 
+  }
   res.end();
 });
 
